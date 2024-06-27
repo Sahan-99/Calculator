e91c35f96798cfbdc443aff63dc7060446419331
Sahan Sandaruwan <sahansandaruwan21@gmail.com>
2024-06-27 13:56:28 +0530

    first commit

---
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author Sahan
+ */
+public class Calculator{
+
+    public static void main(String[] args) {
+   
+    }
+}
